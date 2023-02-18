99eea39a346b3ccf5e946cee20266ee6048d1b8f
niyas <hojaniyazhan@yahoo.com>
2023-02-18 23:50:26 +0100

    is palindrome

---
+++ is-palindrome.js
@@ -0,0 +1,23 @@
+// CHALLENGE 2: VALIDATE A PALINDROME
+// Return true if palindrome and false if not
+// ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
+
+function isPalindrome(str) {
+  const revString = str.split('').reverse().join('');
+
+  return revString === str;
+
+  ///////////////////////
+
+//     let isPali = true;
+//     let caseStr = str.toLowerCase()
+//     let arr = caseStr.split('')
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] !== arr[arr.length -1 -i]){
+//             isPali = false;
+//             break;
+//         }
+//     }
+//     return isPali
+
+}
